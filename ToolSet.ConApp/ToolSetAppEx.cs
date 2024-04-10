a683355dd9d26b8477ff88b19adb6319d7a98160
ggehrer <g.gehrer@hotmail.com>
2024-04-10 09:32:55 +0200

    Classes marked as static.

--- ToolSet.ConApp/ToolSetAppEx.cs
+++ ToolSet.ConApp/ToolSetAppEx.cs
@@ -4,7 +4,7 @@
     {
         static partial void ClassConstructing()
         {
-            SourcePath = "/Users/ggehrer/source/repos/leoggehrer/2324-34_ABIF_ACIF_POSE";
+//            SourcePath = "/Users/ggehrer/source/repos/leoggehrer/2324-34_ABIF_ACIF_POSE";
         }
         static partial void ClassConstructed()
         {
